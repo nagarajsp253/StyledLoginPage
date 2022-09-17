6624e321e38714e51a5d4872dced8e78a0d6abd2
NagarajSP <nagarajpatagar97@gmail.com>
2022-09-17 11:39:52 +0530

    testing project 55

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,13 @@ const Home = () => {
   return (
     <div>
       <p>I am Home page</p>
+      <div>
+        <img src="../Images/home.png" alt="png image" width="200" height="200" />
+        <img src="../Images/logo.webp" alt="webp image" width="200" height="200" />
+        <img src="../Images/home.jfif" alt="jfif image" width="200" height="200" />
+        <img src="../Images/hero.svg" alt="svg image" width="200" height="200" />
+        <img src="../Images/home.jpg" alt="jpg image" width="200" height="200" />
+      </div>
     </div>
   )
 }
