55cc169160abc0e982e4dc8b8c991beb8049e17a
NagarajSP <nagarajpatagar97@gmail.com>
2022-09-17 12:02:38 +0530

    testing project 99.2

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
     <div>
       <p>I am Home page</p>
       <div>
-        <img src={require('./home.png')} alt="png image" width="200" height="200" />
+        <img src={require('./assets/home.png')} alt="png image" width="200" height="200" />
         <img src={require('./logo.webp')} alt="webp image" width="200" height="200" />
         <img src={require('./home.jfif')} alt="jfif image" width="200" height="200" />
         <img src={require('./hero.svg')} alt="svg image" width="200" height="200" />
-        <img src={require('./home.jpg')} alt="jpg image" width="200" height="200" />
+        <img src={require('./assets/home.jpg')} alt="jpg image" width="200" height="200" />
       </div>
     </div>
   )
