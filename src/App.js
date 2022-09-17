4fc6ee767b579e8e081c759a6607b0791217e536
NagarajSP <nagarajpatagar97@gmail.com>
2022-09-17 11:29:20 +0530

    testing project 44

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
             <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
             <Route path="/contact" element={<Contact />}/>
-            <Route path="*" element={<Error />}/>
+            {/* <Route path="*" element={<Error />}/> */}
          </Routes>
       <Footer/>
        </BrowserRouter>
